91241660be97881f7012cb62bfe270910bbd1450
levodigital <86460336+levodigital@users.noreply.github.com>
2021-06-30 23:04:41 +1000

    Update Logo.tsx

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from "react"
 const Logo = () => {
   return (
-  <img style="height: 35px;" class="header-logo" src="https://www.levo.com.au/Static/assets/images/Levo-Logo-2020-Purple.svg" alt="Levo">
+  
   )
 }
 
